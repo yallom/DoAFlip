7f2a740ac89d60a9b54e72696c6740ed88f7c196
JoaoCunha50 <joaomanuelmachadodacunha@gmail.com>
2026-02-14 15:40:34 +0000

    setup mobile app

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,32 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+import { useColorScheme } from 'react-native';
+
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export const unstable_settings = {
+    anchor: '(tabs)',
+};
+
+export default function RootLayout() {
+    const colorScheme = useColorScheme();
+
+    return (
+        <GluestackUIProvider mode="dark">
+            <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+                <SafeAreaProvider>
+                    <Stack>
+                        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+                    </Stack>
+                    <StatusBar style="auto" />
+                </SafeAreaProvider>
+            </ThemeProvider>
+        </GluestackUIProvider>
+    );
+}
