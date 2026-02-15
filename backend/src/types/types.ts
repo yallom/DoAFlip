6b26d91cdbafc4b2ef03c34fcb9e850b457b1143
yallom <alctrindade2005@gmail.com>
2026-02-15 01:24:52 +0000

    feat: initial typing, left unfinished

--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -24,5 +24,21 @@ interface Utilizador {
   updated_at: Date;
 }
 
+type TipoRefeicao = "pequeno_almoco" | "almoco" | "jantar" | "lanche"
+
+interface Refeicao {
+  id: string;
+  planoAlimentar_id: string;
+  tipo: TipoRefeicao;
+  data: Date;
+  data_nascimento: Date;
+  altura_cm: number;
+  peso_kg: number;
+  genero: Genero;
+  objetivo: Objetivo;
+  created_at: Date;
+  updated_at: Date;
+}
+
 export { Genero, Objetivo };
 export type { Utilizador };
